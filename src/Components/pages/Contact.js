f5ccee549d521d7b3eb4583c484140f761e62284
Luqman Ahmed <khawajaluqman98@gmail.com>
2024-01-19 12:45:47 +0200

    first commit

---
+++ src/Components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>YOU ARE ON THE CONTACT PAGE</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/pages/Service.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div>
+        <h1>YOU ARE ON THE SERVICES PAGE</h1>
+
+    </div>
+  )
+}
+
+export default Service
